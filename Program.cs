2becae94c21a8fd73aa8d7236c6b45e47610b21d
pato <pato.araya.gonzalez@gmail.com>
2020-05-28 23:44:54 -0400

    feat(program): add Host configuration

--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace fivet_server
+namespace Fivet.Server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        
+        /// <summary>
+        /// Build and configure Host
+        /// </summary>
+        /// <param name="args"> Arguments to be passed</param> 
+        /// <returns> IHostBuilder</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args){
+
+            return Host
+            .CreateDefaultBuilder(args)
+            .UseEnvironment("Development")
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.TimestampFormat = "[yyyy-MM-dd HH:mm:ss.fff] ";
+                    options.DisableColors = false;
+                });
+                logging.SetMinimumLevel(LogLevel.Trace);
+            })
+            .UseConsoleLifetime()
+            .ConfigureServices((hostContext, services)=>
+            {
+                // FivetService
+                services.AddHostedService<FivetService>();
+                // Logging Service
+                services.AddLogging();
+                // Add 15s wait
+                services.Configure<HostOptions>(option =>
+                {
+                    option.ShutdownTimeout = System.TimeSpan.FromSeconds(15);
+                });
+            });
         }
     }
 }
